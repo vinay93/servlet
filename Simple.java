188c67e4e958f8959ef0ac37b0308e95cddb6f39
vinay93 <vinay.pandravada@gmail.com>
2015-04-04 11:28:32 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -12,9 +12,11 @@ public class Simple extends HttpServlet {
 
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
+		String n=request.getParameter("userName");  
+    		String p=request.getParameter("userPass");  
 		
-		String p=request.getParameter("userPass");
-		if(p.equals("servlet")){
+		if(p.equals("servlet"))
+		{
 			RequestDispatcher rd=request.getRequestDispatcher("welcome");
 			rd.forward(request, response);
 			
